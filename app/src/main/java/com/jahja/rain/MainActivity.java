0b097766b348edc55c5531c92e26fbc2d0a590b7
njahja <nicholasjahja@gmail.com>
2018-08-04 04:10:27 -0700

    location permission update

--- app/src/main/java/com/jahja/rain/MainActivity.java
+++ app/src/main/java/com/jahja/rain/MainActivity.java
@@ -1,13 +1,103 @@
 package com.jahja.rain;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient mFusedLocationClient;
+    TextView test;
+    Button refreshButton;
+    ProgressBar progressBar;
+    Task<Location> task;
+    Location currentLocation;
+    int counter = 0;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test = (TextView) findViewById(R.id.textView);
+        refreshButton = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        //get instance of Fused Location Provider Client
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        myCheckPermissions();
+
     }
+
+    public void myCheckPermissions() {
+        //check permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission granted
+                } else {
+                    finish();
+                }
+            }
+        }
+    }
+
+    public void getWeatherData(View view) {
+        task = mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                currentLocation = task.getResult();
+                test.setText(currentLocation.toString());
+            }
+        });
+    }
+
+//    class FetchWeatherData extends AsyncTask<Void, Void, Void> {
+//        @Override
+//        protected void onPreExecute() {
+//            progressBar.setVisibility(View.VISIBLE);
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected Void doInBackground(Void... voids) {
+//            task = mFusedLocationClient.getLastLocation();
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void aVoid) {
+//            progressBar.setVisibility(View.INVISIBLE);
+//            currentLocation = task.getResult();
+//            test.setText(currentLocation.toString());
+//            super.onPostExecute(aVoid);
+//        }
 }
